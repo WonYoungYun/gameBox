294c7805a86e9ffae2356af68081ef1046593f20
wonyoung <asmar20288@naver.com>
2019-02-04 19:29:57 +0900

    mul Css

--- gamebox/src/components/Multiplication.vue
+++ gamebox/src/components/Multiplication.vue
@@ -1,30 +1,38 @@
 <template>
-  <div>
-    <h1>구구단 게임</h1>
-    <div>User Point : {{points}}</div>
-    <div>{{firstNum}} X {{secondNum}}</div>
-    <input type="text" v-model="answerNum" ref="answerNum" @keyup.enter="submitAnswer">
-    <div class="ket-pad">
-      <div>
+  <div class="main-content">
+    <h2 class="game-title">구구단</h2>
+    <div class="game-area">
+    <div class="game-status">
+        <div class="msg">{{msg}}</div>
+        <div class="points">당신의 점수 : {{points}}</div>
+        <div class="mul-question">{{firstNum}} X {{secondNum}}</div>
+    </div>
+    <div class="calculator">
+    <input type="text " v-model="answerNum" ref="answerNum" @keyup.enter="submitAnswer" class="input-num" disabled>
+    <div class="key-pad">
+      <div class="pad">
         <button @click="getNum">7</button>
         <button @click="getNum">8</button>
         <button @click="getNum">9</button>
       </div>
-      <div>
+      <div class="pad">
         <button @click="getNum">4</button>
         <button @click="getNum">5</button>
         <button @click="getNum">6</button>
       </div>
-      <div>
+      <div class="pad">
         <button @click="getNum">1</button>
         <button @click="getNum">2</button>
         <button @click="getNum">3</button>
       </div>
-      <div>
+      <div class="pad">
+        <button type="reset" @click.prevent="resetAnswer">&times;</button>
         <button @click="getNum">0</button>
-        <button type="submit" @click.prevent="submitAnswer">제출</button>
-        <button type="reset" @click.prevent="resetAnswer">삭제</button>
+        <button type="submit" @click.prevent="submitAnswer">&equals;</button>
+
       </div>
+    </div>
+    </div>
     </div>
   </div>
 </template>
@@ -36,7 +44,8 @@ export default {
       fisrtNum: 0,
       secondNum: 0,
       answerNum: "",
-      points: 0
+      points: 0,
+      msg: ""
     };
   },
   created() {
@@ -67,13 +76,13 @@ export default {
       else this.failMul();
     },
     correctMul() {
-      alert("정답!");
+      this.msg = "정답! +1점"
       this.increasePoints();
       this.resetAnswer();
       this.setFirstMul();
     },
     failMul() {
-      alert("틀렸습니다.");
+      this.msg = "틀렸습니다! -1점"
       this.decreasePoints();
       this.resetAnswer();
       this.setFirstMul();
@@ -92,5 +101,65 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.main-content {
+  margin: 100px auto;
+  padding: 70px 100px;
+  width: 1000px;
+  min-height: 600px;
+  background-color: #fff;
+  box-sizing: border-box;
+  user-select: none;
+}
+.game-area{
+  text-align: center;
+}
+.msg{
+  height:50px;
+  color: rgb(255, 0,0);
+  font-size:24px;
+  font-weight: 700;
+}
+.points{
+  font-size: 22px;
+}
+.mul-question{
+  font-size: 30px;
+  font-weight: 700;
+}
+.calculator{
+  display: inline-block;
+  margin-top: 20px;
+  width: 300px;
+  height: 430px;
+  border:2px solid #0000cd;
+  background-color: #efefef;
+}
+.input-num{
+  margin: 30px 0 20px 0;
+  width: 100%;
+  height: 50px;
+  font-size:24px;
+  font-weight: 700;
+  border:none;
+  outline:none;
+  text-align: center;
+}
+.input-num:disabled{
+  background-color:#fff;
+  color:#333;
+}
+.key-pad{
+  margin-top: 5px;
+}
+
+.pad button{
+  margin: 1px 1px;
+  width: 70px;
+  height: 70px;
+  font-size:2em;
+  background-color:#fff;
+  outline:none;
+  border:none;
+}
 </style>
